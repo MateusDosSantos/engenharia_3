446408f67abb15e954ff53a6b968391212e3f439
mateussantos <mateus.santos@ifsuldeminas.edu.br>
2026-05-14 09:45:58 -0300

    14/05 - Drawer

--- app/src/main/java/com/example/engenharia4/Tela02.java
+++ app/src/main/java/com/example/engenharia4/Tela02.java
@@ -7,28 +7,43 @@ import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
 public class Tela02 extends AppCompatActivity {
     private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private NavigationView navigation;
+    private LinearLayout layout;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela02);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        drawer = findViewById(R.id.main1);
+        navigation = findViewById(R.id.nav1);
+        layout = findViewById(R.id.id_layout);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.id_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -49,6 +64,29 @@ public class Tela02 extends AppCompatActivity {
                         startActivity(new Intent(Tela02.this, Tela03.class));
                     }
                 }, 2000);
+            }
+        });
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(
+                Tela02.this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId()== R.id.id_item1){
+                    Toast.makeText(Tela02.this, "Opção1", Toast.LENGTH_SHORT).show();
+                }
+                if(menuItem.getItemId()== R.id.id_item2){
+                    Toast.makeText(Tela02.this, "Opção2", Toast.LENGTH_SHORT).show();
+                }
+                if(menuItem.getItemId()== R.id.id_item3){
+                    Toast.makeText(Tela02.this, "Opção3", Toast.LENGTH_SHORT).show();
+                }
+                if(menuItem.getItemId()== R.id.id_item4){
+                    Toast.makeText(Tela02.this, "Opção4", Toast.LENGTH_SHORT).show();
+                }
+                return false;
             }
         });
 
